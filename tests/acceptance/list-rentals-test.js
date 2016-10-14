a8a72ca401087d3eeec8c211e34f467e6b74e288
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-10-13 23:44:57 -0700

    updated the acceptance test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,10 +3,35 @@ import moduleForAcceptance from 'ember-quickstart/tests/helpers/module-for-accep
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('visiting /list-rentals', function(assert) {
-  visit('/list-rentals');
+test('visiting /', function(assert) {
+  visit('/');
 
   andThen(function() {
-    assert.equal(currentURL(), '/list-rentals');
+    assert.equal(currentURL(), '/');
   });
+});
+
+
+test('should redirect to rentals route', function(assert) {
+
+});
+
+test('should list available rentals', function(assert) {
+
+});
+
+test('should link to information about the company', function(assert) {
+
+});
+
+test('should link to contact information', function(assert) {
+
+});
+
+test('should filter the list of rentals by city', function(assert) {
+
+});
+
+test('should show details for a specific rental', function(assert) {
+
 });
